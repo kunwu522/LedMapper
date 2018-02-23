f8291a280945d84b98c6ed3c020b2c30ff9665e9
TonyNumber5 <wukun522@gmail.com>
2018-02-24 01:14:26 +0800

    Added multiple mode for test

--- Arduino/LedMapperTest/LedMapperTest.ino
+++ Arduino/LedMapperTest/LedMapperTest.ino
@@ -1,8 +1,5 @@
 #include "FastLED.h"
 
-#define DW_LED_PACKET_HEADER 0xFF
-#define DW_LED_DATA 0xBE
-
 #define LEDS_NUM  16
 #define STRIPS_NUM  8
 
@@ -16,14 +13,33 @@
 #define DATA_PIN5 5
 #define DATA_PIN6 6
 #define DATA_PIN7 7
+#define DATA_PIN8 8
+#define DATA_PIN9 9
+#define DATA_PIN10 10 
+#define DATA_PIN11 11
+#define DATA_PIN12 12
+#define DATA_PIN13 13
+#define DATA_PIN14 14
+#define DATA_PIN15 15
+#define DATA_PIN16 16
+#define DATA_PIN17 17
+#define DATA_PIN18 18
+#define DATA_PIN19 19
+#define DATA_PIN20 20
+#define DATA_PIN21 21
 
 CRGB ledStrips[STRIPS_NUM][LEDS_NUM];
 
 char data[STRIPS_NUM * (3 + 1)];
 
+int mode = 0;
+
 void setup() {
-//  Serial.begin(9600);
   Serial.setTimeout(50);
+
+  #ifdef TEST_MODE
+    Serial.begin(115200);
+  #endif
   
   FastLED.addLeds<WS2812B, DATA_PIN0, RGB>(ledStrips[0], LEDS_NUM);
   FastLED.addLeds<WS2812B, DATA_PIN1, RGB>(ledStrips[1], LEDS_NUM);
@@ -47,24 +63,59 @@ void setup() {
 //  FastLED.addLeds<WS2812B, DATA_PIN19, RGB>(ledStrips[19], LEDS_NUM);
 //  FastLED.addLeds<WS2812B, DATA_PIN20, RGB>(ledStrips[20], LEDS_NUM);
 //  FastLED.addLeds<WS2812B, DATA_PIN21, RGB>(ledStrips[21], LEDS_NUM);
+
+  FastLED.setDither(0);
 }
 
 void loop() {
+  byte startByte = Serial.read();
+  
+  if (startByte == '#') { // Change Running Mode
+    mode = Serial.read();
+    #ifdef TEST_MODE
+      Serial.println("########### Changed to %d mode successfully", mode);
+    #endif
+  }
+
+  switch (mode) {
+    case 0: // Normal Running
+      processingLoop();
+      break;
+    case 1: // Test Leds
+      testAllLeds();
+      break;
+    case 2:
+      testSingleLed();
+      break;
+    case 3:
+      testMultipleLeds();
+      break;
+    default:
+      break;
+  }
+}
+
+void showLed() {
+  char *dataPtr = data;
+  for (int i = 0; i < STRIPS_NUM; i++) {
+    CRGB color = CRGB(dataPtr[1], dataPtr[2], dataPtr[3]);
+    fill_solid(ledStrips[i], LEDS_NUM, color);
+    FastLED[i].showLeds(data[0]);
+    dataPtr += 4;
+  }
+}
+
+void processingLoop() {
   #ifdef TEST_MODE
-    for (int i = 0; i < STRIPS_NUM; i++) {
-      fill_solid(ledStrips[i], LEDS_NUM, CRGB::Red);
-    }
-    FastLED.setBrightness(128);
-    FastLED.show();
-  #else
-    byte startByte = Serial.read();
+    Serial.println("Standard Running, waiting data...");
+  #endif
+  byte startByte = 0;
+  while (Serial.available() > 0) {
+    startByte = Serial.read();
+  }
   if (startByte == '*') {
-//    fill_solid(ledStrips[7], LEDS_NUM, CRGB::Black);
-//    FastLED.setBrightness(128);
-//    FastLED.show();
     int count = Serial.readBytes((char *)data, sizeof(data));
     if (count == sizeof(data)) {
-//      Serial.println(data[8]);
       showLed();
     } else {
       fill_solid(ledStrips[7], LEDS_NUM, CRGB::Blue);
@@ -111,16 +162,78 @@ void loop() {
 //    FastLED.setBrightness(128);
     FastLED.show();
   }
-  #endif
 }
 
-void showLed() {
-  char *dataPtr = data;
+void testAllLeds() {
+  #ifdef TEST_MODE
+    Serial.println("Test All Leds...");
+  #endif
   for (int i = 0; i < STRIPS_NUM; i++) {
-    CRGB color = CRGB(dataPtr[1], dataPtr[2], dataPtr[3]);
-    fill_solid(ledStrips[i], LEDS_NUM, color);
-    FastLED[i].showLeds(data[0]);
-    dataPtr += 4;
+    fill_solid(ledStrips[i], LEDS_NUM, CRGB::Red);
+  }
+  FastLED.setBrightness(128);
+  FastLED.show();
+}
+
+void testSingleLed() {
+  #ifdef TEST_MODE
+    Serial.println("Test Single Led, waiting serial input");
+  #endif
+  byte dataLength = getFirstData();
+  if (dataLength == 0) {
+    return;
+  }
+  
+  char newData[dataLength];
+  int count = Serial.readBytes((char *)newData, sizeof(newData));
+  if (count != dataLength) {
+    #ifdef TEST_MODE
+      Serial.println("Error, Input data lenght is not valid.");
+    #endif
+    return;
+  }
+
+  short id = data[0];
+  short bright = data[1];
+  CRGB c = CRGB(data[2], data[3], data[4]);
+  fill_solid(ledStrips[id], LEDS_NUM, c);
+  FastLED.setBrightness(bright);
+  FastLED.show();
+}
+
+void testMultipleLeds() {
+  #ifdef TEST_MODE
+    Serial.println("Test Multiple LEDs, waiting serial input");
+  #endif
+
+  byte dataLength = getFirstData();
+  if (dataLength == 0) {
+    return;
+  }
+
+  char newData[dataLength];
+  int count = Serial.readBytes((char *)newData, sizeof(newData));
+  if (count != dataLength) {
+    #ifdef TEST_MODE
+      Serial.println("Error, Input data length is not valid");
+    #endif
+    return;
+  }
+
+  for (int i = 0; i < dataLength; i+=5) {
+    short id = data[i];
+    short bright = data[i + 1];
+    CRGB c = CRGB(data[i + 2], data[i + 3], data[i + 4]);
+    fill_solid(ledStrips[id], LEDS_NUM, c);
+    FastLED[id].showLeds(bright);
+  }
+}
+
+byte getFirstData() {
+  if (Serial.available() > 0) {
+    return Serial.read();
+  } else {
+    return 0;
   }
 }
 
